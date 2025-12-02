3dd7212615bb66e80ef12037d32fd0f7245ec431
Sharon <sharon.bhosle@ellume.net>
2025-12-02 19:31:14 +0530

    completed numbers from built-ins section

---
+++ numbers.dart
@@ -0,0 +1,17 @@
+
+void main() {
+  // int a = 2;
+  // int? b = 2;
+  // double a = 1;
+  // print(a);  // 1.0 weir dei data type ist double
+
+  // num a = 1.0; // wenn dei data type ist num es kann sei int oder double, wir habt zu worry nicht
+
+  // final API_result ; final ist venwerden wann dei value ist set during run-time, fur example ein value welche wir erhalten von 3rd party API
+  //
+  // const pi = 3.14; const ist verwerden wann dei value ist set during compile-time, es ist wie normal constant von mathmatics
+  // verwerdenst dei final keyword fur die run-time value, wenn verwerdenst im compile es will gibt error
+
+  // const current_time  = DateTime.now(); //datetime.now() geben value am run-time, somit es will gibt ein error
+
+}
